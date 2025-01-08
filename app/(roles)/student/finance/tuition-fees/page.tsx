d02f9266de55f63cf892766a88c357b93b8d1803
rasya109 <satriavega2004@gmail.com>
2025-01-08 08:30:13 +0700

    feature(school fee): added function to make input convert to Rp

--- app/(roles)/student/finance/tuition-fees/page.tsx
+++ app/(roles)/student/finance/tuition-fees/page.tsx
@@ -3,13 +3,25 @@
 import React from "react";
 import "@/app/styles/globals.css";
 import Image from 'next/legacy/image';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { roleMiddleware } from "@/app/(auth)/middleware/middleware";
 
 export default function StudentTuitionFeesPage() {
+  const [payment, setPayment] = useState<string>("");
+
+  const formatToRupiah = (value: string): string => {
+    if (!value) return "";
+    return "Rp " + value.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  };
+
+  const handlePaymentChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setPayment(formatToRupiah(e.target.value));
+  };
+
   useEffect(() => {
     // Panggil middleware untuk memeriksa role, hanya izinkan 'Student'
     roleMiddleware(["Student"]);
+
   }, []);
   return (
     <div className="flex-1 flex flex-col overflow-hidden bg-gray-50">
@@ -70,7 +82,9 @@ export default function StudentTuitionFeesPage() {
                 <input
                   type="text"
                   id="jumlah"
-                  placeholder="600.000"
+                  placeholder="Rp 600.000"
+                  value={payment}
+                  onChange={handlePaymentChange}
                   className="mt-2 block w-full rounded-md border border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm px-4 py-2 text-gray-700 bg-white"
                 />
               </div>
